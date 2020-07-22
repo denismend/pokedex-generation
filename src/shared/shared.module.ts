19ad4c312ff546c586e370583aa603ad6ef5fa4b
Denis M. Ladeira <denisladeira1@gmail.com>
2020-07-22 20:13:52 -0300

    layout changes

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
 import { MatCardModule } from '@angular/material';
+import { DarkenOnHoverModule } from './directives/dark-on-hover/dark-on-hover.module';
 
 @NgModule({
   declarations: [
     PokemonCardComponent
   ],
   exports: [
-    PokemonCardComponent
+    PokemonCardComponent,
   ],
   imports: [
     CommonModule,
+
+    DarkenOnHoverModule,
 
     MatCardModule,
   ],
